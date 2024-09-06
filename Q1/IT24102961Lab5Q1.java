9f70105d93f085a5316b1a3ab10bf3d928dda63c
IT24102961 <it24102961@my.sliit.lk>
2024-09-06 20:08:55 +0530

    lab5q1

---
+++ Q1/IT24102961Lab5Q1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class IT24102961Lab5Q1{
+	public static void main(String[] args){
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the first integer: ");
+		int num1=scanner.nextInt();
+		System.out.print("Enter the second integer: ");
+        	int num2=scanner.nextInt();        
+        	System.out.print("Enter the third integer: ");
+        	int num3=scanner.nextInt();
+        	scanner.close();
+
+        	System.out.println("\n\nUser entered numbers are: "+num1+" "+num2+" "+num3);
+
+        	int smallest=num1;
+        	int largest=num1;
+		if(num2<smallest){
+            		smallest=num2;
+        	}
+        	if(num3<smallest){
+            		smallest=num3;
+        	}
+        
+        	if(num2>largest){
+            		largest=num2;
+        	}
+        	if(num3>largest){
+            		largest=num3;
+        	}
+        	System.out.println("The Smallest number is: "+smallest);
+        	System.out.println("The Largest number is: "+largest);
+	}
+}
